e5d89e136824610269881c81936eaea836810997
kimdonghyun <ghffu405@gmail.com>
2025-03-27 16:16:44 +0900

    init

---
+++ src/main/java/com/unstage/api/UnstageApplication.java
@@ -0,0 +1,13 @@
+package com.unstage.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnstageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnstageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unstage/api/UnstageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unstage.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnstageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
