df25129deb71e9454965a73bc94e3b53b8641272
kimdonghyun <ghffu405@gmail.com>
2025-03-31 00:35:09 +0900

    modify run_new_was.sh

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -23,6 +23,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/nginx/playground-logging/build/libs/* > /home/ubuntu/nginx/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/unstage/api/build/libs/* > /home/ubuntu/logs/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
