ec17fd602ea4017d6b9e7c7fe318309d2d98bd01
kimdonghyun <ghffu405@gmail.com>
2025-03-31 00:23:57 +0900

    testing

--- api/src/main/java/com/unstage/api/TestController.java
+++ api/src/main/java/com/unstage/api/TestController.java
@@ -3,13 +3,23 @@ package com.unstage.api;
 import com.unstage.core.AEntity;
 import com.unstage.core.AService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${unstage-version.version}")
+    private String version;
+
     @Autowired
     private AService aService;
+
+    @GetMapping("/")
+    public String version() {
+        return String.format("Project Version: %s", version);
+    }
 
     @GetMapping("/test")
     public String test() {
@@ -22,4 +32,6 @@ public class TestController {
     public String checkHealth() {
         return "healthy";
     }
+
+
 }
