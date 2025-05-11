06b4e70c475d421e4c7aff79596ee0f74998c10f
kimdonghyun <ghffu405@gmail.com>
2025-05-11 23:34:48 +0900

    chore: TODO 주석 제거

--- api/src/main/java/com/unstage/api/controller/InstructorController.java
+++ api/src/main/java/com/unstage/api/controller/InstructorController.java
@@ -2,9 +2,6 @@ package com.unstage.api.controller;
 
 import com.unstage.core.instructor.dto.CreatePortfolioRequest;
 import com.unstage.core.instructor.dto.GetPortfoliosResponse;
-import com.unstage.core.instructor.dto.InstructorRequest;
-import com.unstage.core.instructor.dto.InstructorResponse;
-import com.unstage.core.instructor.entity.Instructor;
 import com.unstage.core.instructor.service.InstructorService;
 import com.unstage.core.instructor.service.PortfolioService;
 import lombok.RequiredArgsConstructor;
@@ -22,11 +19,11 @@ public class InstructorController {
     private final InstructorService instructorService;
     private final PortfolioService portfolioService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<InstructorResponse> getInstructor(@PathVariable final Long id) {
-        final Instructor instructor = instructorService.getInstructor(id);
-        return ResponseEntity.ok(InstructorResponse.from(instructor));
-    }
+//    @GetMapping("/{id}")
+//    public ResponseEntity<InstructorResponse> getInstructor(@PathVariable final Long id) {
+//        final Instructor instructor = instructorService.getInstructor(id);
+//        return ResponseEntity.ok(InstructorResponse.from(instructor));
+//    }
 
     // 코드 리뷰 완료
     @GetMapping("/{instructorId}/portfolios")
@@ -46,42 +43,42 @@ public class InstructorController {
                 .fromPath("/api/v1/portfolios/{portfolioId}")
                 .buildAndExpand(portfolioId)
                 .toUri();
-        //TODO 303 응답으로 변경
+
         return ResponseEntity.created(location).build();
     }
 
-    @PostMapping
-    public ResponseEntity<InstructorResponse> createInstructor(@RequestBody final InstructorRequest request) {
-        final Instructor instructor = instructorService.createInstructor(
-                request.name(),
-                request.email(),
-                request.bio()
-        );
-
-        final URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(instructor.getId())
-                .toUri();
-
-        return ResponseEntity.created(location).body(InstructorResponse.from(instructor));
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<InstructorResponse> updateInstructor(
-            @PathVariable final Long id,
-            @RequestBody final InstructorRequest request) {
-        final Instructor instructor = instructorService.updateInstructor(
-                id,
-                request.name(),
-                request.bio()
-        );
-        return ResponseEntity.ok(InstructorResponse.from(instructor));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteInstructor(@PathVariable final Long id) {
-        instructorService.deleteInstructor(id);
-        return ResponseEntity.noContent().build();
-    }
+//    @PostMapping
+//    public ResponseEntity<InstructorResponse> createInstructor(@RequestBody final InstructorRequest request) {
+//        final Instructor instructor = instructorService.createInstructor(
+//                request.name(),
+//                request.email(),
+//                request.bio()
+//        );
+//
+//        final URI location = ServletUriComponentsBuilder
+//                .fromCurrentRequest()
+//                .path("/{id}")
+//                .buildAndExpand(instructor.getId())
+//                .toUri();
+//
+//        return ResponseEntity.created(location).body(InstructorResponse.from(instructor));
+//    }
+//
+//    @PutMapping("/{id}")
+//    public ResponseEntity<InstructorResponse> updateInstructor(
+//            @PathVariable final Long id,
+//            @RequestBody final InstructorRequest request) {
+//        final Instructor instructor = instructorService.updateInstructor(
+//                id,
+//                request.name(),
+//                request.bio()
+//        );
+//        return ResponseEntity.ok(InstructorResponse.from(instructor));
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Void> deleteInstructor(@PathVariable final Long id) {
+//        instructorService.deleteInstructor(id);
+//        return ResponseEntity.noContent().build();
+//    }
 }
